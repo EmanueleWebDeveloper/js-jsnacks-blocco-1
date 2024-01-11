6cf79561bdaf0893591b2949f646970e9853640a
EmanueleWebDeveloper <emanuelelucacali@gmail.com>
2024-01-11 16:06:53 +0100

    js 1 esercizio

--- blocco1/snack1/main.js
+++ blocco1/snack1/main.js
@@ -2,11 +2,20 @@
 // Il software deve chiedere per 10 volte all’utente di inserire un numero.
 // Il programma stampa la somma di tutti i numeri inseriti.
 
-let numero = prompt( "scegli un numero ")
 
-console.log( numero)
+let numero = prompt("Scegli un numero ");
+let somma = 0;
 
-for( let i = 0; i = 10; i++)
+
+numero = +numero;
+
+for (let i = 1; i <= 10; i++) {
+    let input = prompt("Inserisci un numero");
+
+    somma += +input;
+}
+
+console.log("La somma totale è: " + somma);
 
 
 
