1f2a8d9da0c98ec7d9170d03c54d5ddcc235cd3c
EmanueleWebDeveloper <emanuelelucacali@gmail.com>
2024-01-11 18:05:52 +0100

    es4

--- blocco1/snack2/main2.js
+++ blocco1/snack2/main2.js
@@ -1,3 +1,17 @@
-Blocco 1 - snake 6
--Stampa il cubo dei primi N numeri,
-dove N è un numero indicato dall’utente.
+// Blocco 1 - snake 6
+// -Stampa il cubo dei primi N numeri,
+// dove N è un numero indicato dall’utente.
+
+
+let Numero = prompt("Inserisci un numero:")
+let resultHtml = document.getElementById("cubiNumeri")
+
+for (let i = 1; i <= Numero; i++) {
+
+    let calcoloCubo = [i] * [i] * [i]
+
+    console.log(calcoloCubo)
+
+    resultHtml.innerHTML += `Cubo: ${calcoloCubo}, `
+}
+
