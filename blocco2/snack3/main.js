14d613da143d67907e2c7286ad8760ad1334df56
EmanueleWebDeveloper <emanuelelucacali@gmail.com>
2024-01-11 16:57:17 +0100

    esercizio 3

---
+++ blocco2/snack3/main.js
@@ -0,0 +1,15 @@
+// Blocco 2 - snack 2
+// Generatore di “nomi cognomi” casuali: il Grande Gatsby ha una lista di nomi e una lista di cognomi, e da queste vuole generare una falsa lista di invitati con nome e cognome.
+
+var nomi = ["luca", "marco", "roberto", "lidia", "ylenia", "carlo"];
+var cognomi = ["rossi", "pluto", "marino", "ferrara", "devisu"];
+
+let nomeCognome = [];
+
+for (let i = 0; i < nomi.length; i++) {
+    for (let j = 0; j < cognomi.length; j++) {
+        nomeCognome.push(nomi[i] + " " + cognomi[j]);
+    }
+}
+
+console.log(nomeCognome);
