73c645d7d946c91abbb9b59f8a29ecb882be4858
EmanueleWebDeveloper <emanuelelucacali@gmail.com>
2024-01-11 15:51:11 +0100

    inizio 1 esercizio

---
+++ blocco1/snack1/main.js
@@ -0,0 +1,17 @@
+// Blocco 1 - JSnack 3
+// Il software deve chiedere per 10 volte all’utente di inserire un numero.
+// Il programma stampa la somma di tutti i numeri inseriti.
+
+let numero = prompt( "scegli un numero ")
+
+console.log( numero)
+
+for( let i = 0; i = 10; i++)
+
+
+
+
+
+
+
+
